01c4a996fc4291159bd1ae0ad3b52e52b4c6d0bd
vijendra-kumar <vijendra3mca@gmail.com>
2017-07-06 11:29:57 +0530

    Add files via upload

---
+++ scert/assets/js/index.js
@@ -0,0 +1,8 @@
+ $("#login-button").click(function(event){
+	 event.preventDefault();
+     console.log('In login function');
+     $('form').submit();
+	 //$('form').fadeOut(500);
+	 //$('.wrapper').addClass('form-success');
+	 
+});
